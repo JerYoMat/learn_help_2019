0edfd75f7f4858881491a4282ef9ce0cdfbf6909
JerYoMat <jeremy.emata@gmail.com>
2019-07-12 05:30:03 -0400

    style/refactoring

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,28 +1,26 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-require "minitest/reporters"
+require 'minitest/reporters'
 Minitest::Reporters.use!
-[Minitest::Reporters::SpecReporter.new, Minitest::Reporters::ProgressReporter.new]
 
 class ActiveSupport::TestCase
-  fixtures :all
-  #For model tests to test presence of required attribute
   def missing_data_assertion(instance_var, class_instance)
     class_instance[instance_var] = ''
     sym = instance_var.to_sym
-    refute class_instance.valid?, "user is invalid without #{instance_var}"
+    assert_not class_instance.valid?, "user is invalid without #{instance_var}"
     assert_not_nil class_instance.errors[sym], "no validation error for #{instance_var} present"
-  end 
+  end
 
   def entry_valid?(item, class_instance, instance_var)
-      class_instance[instance_var] = item
-      assert class_instance.valid?, "#{item.inspect} should be valid"
+    class_instance[instance_var] = item
+    assert class_instance.valid?, "#{item.inspect} should be valid"
   end
 
   def entry_invalid?(item, class_instance, instance_var)
     class_instance[instance_var] = item
     assert_not class_instance.valid?, "#{item.inspect} should not be valid"
   end
-
 end
