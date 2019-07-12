e21fdaa012ef6bf35d2c696bdaf5f69ddb3a8a31
JerYoMat <jeremy.emata@gmail.com>
2019-07-12 15:17:31 -0400

    BUG FIX: removed topic  ids, this is handled by TipTopics

--- test/models/learn_resource_test.rb
+++ test/models/learn_resource_test.rb
@@ -2,15 +2,16 @@
 
 require 'test_helper'
 require 'pry'
+
+# Model tests for LearnResources
 class LearnResourceTest < ActiveSupport::TestCase
   setup do
+    user = users(:test_user1)
     @resource = LearnResource.new(
       title: 'test resource',
       url: 'www.someresource.com',
-      user_id: users(:test_user1).id
+      user_id: user.id
     )
-    @topic1 = topics(:one)
-    @topic2 = topics(:two)
   end
 
   teardown do
@@ -31,26 +32,13 @@ class LearnResourceTest < ActiveSupport::TestCase
     assert_not @resource.valid?
   end
 
-  test 'resource has many topics' do
-    @resource.save
-    TipTopic.create(
-      resource_id: @resource.id,
-      topic_id: @topic1.id
-    )
-    TipTopic.create(
-      resource_id: @resource.id,
-      topic_id: @topic2.id
-    )
-
-    assert_equal(2, @resource.topics.size)
-  end
-
   test 'url is unique' do
+    @resource.save 
     @second_resource = LearnResource.new(
       title: 'test resource 2',
       url: 'www.someresource.com',
       user_id: users(:test_user1).id
     )
-    assert_not @second_resource.valid?
+    assert_not @second_resource.save
   end
 end
