1ef4576466bdbdaacb42f2c39622919b64c734f3
JerYoMat <jeremy.emata@gmail.com>
2019-07-12 05:21:51 -0400

    added index on email as this will be the main way users are looked up

--- db/migrate/20190712021010_create_users.rb
+++ db/migrate/20190712021010_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :email
       t.string :github_username
       t.string :password
+      t.string :password_digest
 
       t.timestamps
     end
