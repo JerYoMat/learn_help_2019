b8d86d7b71c7ea8b4e2951a0aed22af02e43ed73
JerYoMat <jeremy.emata@gmail.com>
2019-07-11 22:57:42 -0400

    added generic assertion where class_name is provided and attribute value is set to blank

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,14 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
 
   fixtures :all
-
+  #For model tests to test presence of required attribute
+  def missing_data_assertion(object_attr, class_name)
+    #create instance of class 
+    class_instance = class_name.constantize.new
+    #set desired prop to be equal to ''
+    class_instance[object_attr] = ''
+    sym = object_attr.to_sym
+    refute class_instance.valid?, "user is invalid without #{object_attr}"
+    assert_not_nil class_instance.errors[sym], "no validation error for #{object_attr} present"
+  end 
 end
