84678c19478f62bc72522e24660582717560852b
JerYoMat <jeremy.emata@gmail.com>
2019-07-12 05:28:46 -0400

    added basic user model validations. Associations have not been set yet

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def create; end
+
+  def show; end
+
+  def index; end
+
+  def destroy; end
+
+  def update; end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  validates :name, :email, :github_username, :password, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  before_save { self.email = email.downcase }
+  has_secure_password
+  validates :name, :github_username, presence: true, length: { maximum: 255 }
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :password,
+            presence: true,
+            confirmation: true,
+            length: { minimum: 6 }
 end
